7338af0b0df47757ace0730442ff79b53efb015a
david <dgleal@gmail.com>
2008-06-06 16:58:06 +0100

    Moved plug.rb to its proper place

--- spec/plug_spec.rb
+++ spec/plug_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '..', 'lib', 'plug')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'extlib', 'plug')
 
 describe Plug do
   describe "should allow inclusion" do
