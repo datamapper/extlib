44aee3dfaa610f0ac78449b4b26524f9d5db435e
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-06-12 07:04:30 -0700

    Updated respond_to? to pass along the include_private flag to the array

--- lib/extlib/lazy_array.rb
+++ lib/extlib/lazy_array.rb
@@ -68,7 +68,7 @@ class LazyArray  # borrowed partially from StrokeDB
   end
 
   def respond_to?(method, include_private = false)
-    super || @array.respond_to?(method)
+    super || @array.respond_to?(method, include_private)
   end
 
   private
