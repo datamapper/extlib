cb452d07c8f81797179e4ea27aea27faef6e6ec3
Carl Lerche <carllerche@mac.com>
2008-07-13 13:22:16 -0700

    Removes constant caching from Module#find_const [#454 state:resolved]

--- lib/extlib/module.rb
+++ lib/extlib/module.rb
@@ -1,45 +1,44 @@
 class Module
   def find_const(nested_name)
     if nested_name =~ /^\:\:/
-      Object::__nested_constants__[nested_name]
+      Object::__nested_constants__(nested_name)
     else
       begin
-        self.__nested_constants__[nested_name]
+        self.__nested_constants__(nested_name)
       rescue NameError
-        Object::__nested_constants__[nested_name]
+        Object::__nested_constants__(nested_name)
       end
     end
   end
 
-  protected
-  def __nested_constants__
-    @__nested_constants__ ||= Hash.new do |h,k|
-      klass = self
-      modules = [Object]
-      keys = k.split('::')
+protected
+  # Doesn't do any caching since constants can change with remove_const
+  def __nested_constants__(k)
+    klass = self
+    modules = [Object]
+    keys = k.split('::')
 
-      if keys.first.blank? # We got "::Foo::Bar", so we'll get rid of the opening '::', and only search Object
-        keys.shift
-      else
-        klass.name.split('::').inject(modules) do |ary, elem|
-          ary << ary.last.const_get(elem)
-          ary
-        end
-        modules.reverse!
+    if keys.first.blank? # We got "::Foo::Bar", so we'll get rid of the opening '::', and only search Object
+      keys.shift
+    else
+      klass.name.split('::').inject(modules) do |ary, elem|
+        ary << ary.last.const_get(elem)
+        ary
       end
-
-      modules.each do |m|
-        keys.inject(m) do |group, elem|
-          break unless group.const_defined?(elem)
-          klass = group.const_get(elem)
-        end
-        break unless klass == self
-      end
-
-      raise NameError if klass == Object
-
-      h[k] = klass
+      modules.reverse!
     end
+
+    modules.each do |m|
+      keys.inject(m) do |group, elem|
+        break unless group.const_defined?(elem)
+        klass = group.const_get(elem)
+      end
+      break unless klass == self
+    end
+
+    raise NameError if klass == Object
+
+    klass
   end
 
 end # class Module
--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -36,6 +36,7 @@ describe Module do
   end
 
   it "should not cache unresolvable class string" do
+    pending "This shouldn't be applicable anymore, but hopefully somebody can double check before it is removed"
     lambda { find_const('Foo::Bar::Baz') }.should raise_error(NameError)
     Object::send(:__nested_constants__).has_key?('Foo::Bar::Baz').should == false
   end
@@ -52,6 +53,13 @@ describe Module do
   it "should find nested constants on nested constants" do
     # pending "Implement this"
     Foo::Bar.find_const('Noo::Too')
+  end
+  
+  it "should be able to deal with constants being added and removed" do
+    Object.find_const('Bar') # First we load Bar with find_const
+    Object.module_eval { remove_const('Bar') } # Now we delete it
+    module Bar; end; # Now we redefine it
+    Object.find_const('Bar').should == Bar
   end
 
 end
