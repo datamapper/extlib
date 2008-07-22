1f1b16ed28b1413ead463b7fec1452a4410585d4
Michael S. Klishin <michael@novemberain.com>
2008-07-22 16:09:09 +0300

    More spec examples for Mash.

--- spec/mash_spec.rb
+++ spec/mash_spec.rb
@@ -15,7 +15,7 @@ describe Mash do
     it 'converts all Hash values into Mashes if param is a Hash' do
       mash = Mash.new :hash => @hash
 
-      mash.should be_an_instance_of(Mash)
+      mash["hash"].should be_an_instance_of(Mash)
       # sanity check
       mash["hash"]["hash"].should == "different"
     end
@@ -27,6 +27,42 @@ describe Mash do
     end
   end # describe "#initialize"
 
+
+  
+  describe "#update" do
+    it 'converts all keys into strings when param is a Hash' do
+      mash = Mash.new(@hash)
+      mash.update("starry" => "night")
+      
+      mash.keys.any? { |key| key.is_a?(Symbol) }.should be(false)
+    end
+
+    it 'converts all Hash values into Mashes if param is a Hash' do
+      mash = Mash.new :hash => @hash
+      mash.update(:hash => { :hash => "is buggy in Ruby 1.8.6" })
+      
+      mash["hash"].should be_an_instance_of(Mash)
+    end
+  end # describe "#update"
+
+
+  
+  describe "#[]=" do
+    it 'converts key into string' do
+      mash = Mash.new(@hash)
+      mash[:hash] = { "starry" => "night" }
+      
+      mash.keys.any? { |key| key.is_a?(Symbol) }.should be(false)
+    end
+
+    it 'converts all Hash value into Mash' do
+      mash = Mash.new :hash => @hash
+      mash[:hash] = { :hash => "is buggy in Ruby 1.8.6" }
+      
+      mash["hash"].should be_an_instance_of(Mash)
+    end
+  end # describe "#[]="
+  
 
   
   describe "#key?" do
@@ -66,6 +102,66 @@ describe Mash do
       @mash.member?(:rainclouds).should be(false)
       @mash.member?("rainclouds").should be(false)
     end
-    
   end # describe "#key?"
+
+
+  describe "#dup" do
+    it 'returns instance of Mash' do
+      Mash.new(@hash).dup.should be_an_instance_of(Mash)
+    end
+
+    it 'preserves keys' do
+      mash = Mash.new(@hash)
+      dup  = mash.dup
+
+      mash.keys.sort.should == dup.keys.sort
+    end
+
+    it 'preserves value' do
+      mash = Mash.new(@hash)
+      dup  = mash.dup
+
+      mash.values.sort.should == dup.values.sort
+    end
+  end
+
+
+
+  describe "#to_hash" do
+    it 'returns instance of Mash' do
+      Mash.new(@hash).to_hash.should be_an_instance_of(Hash)
+    end
+
+    it 'preserves keys' do
+      mash = Mash.new(@hash)
+      converted  = mash.to_hash
+
+      mash.keys.sort.should == converted.keys.sort
+    end
+
+    it 'preserves value' do
+      mash = Mash.new(@hash)
+      converted = mash.to_hash
+
+      mash.values.sort.should == converted.values.sort
+    end    
+  end
+
+
+
+  describe "#delete" do
+    it 'converts Symbol key into String before deleting' do
+      mash = Mash.new(@hash)
+
+      mash.delete(:hash)
+      mash.key?("hash").should be(false)
+    end
+
+    it 'works with String keys as well' do
+      mash = Mash.new(@hash)
+
+      mash.delete("mash")
+      mash.key?("mash").should be(false)
+    end
+  end
 end
