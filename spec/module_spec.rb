a2a3b15058b2363438385c3116caaead3737306d
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-07-22 22:31:22 -0700

    Remove unecessary code from specs

--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
-require 'pp'
 
 describe Module do
 
@@ -51,7 +50,6 @@ describe Module do
   end
 
   it "should find nested constants on nested constants" do
-    # pending "Implement this"
     Foo::Bar.find_const('Noo::Too')
   end
 
