0561b52aadf3e03222c07f0aab494d7ddb5e07c7
Sam Smoot <ssmoot@gmail.com>
2008-09-08 16:29:34 -0500

    Small tweaks to specs to make them pass and be more portable.

--- spec/datetime_spec.rb
+++ spec/datetime_spec.rb
@@ -3,12 +3,12 @@ require 'json'
 
 describe DateTime, "#to_time" do
   before do
-    @expected = "Wed Aug 20 02:16:06 -0700 2008"
-    datetime = DateTime.parse(@expected)
+    @expected = Time.now.to_s
+    @datetime = DateTime.parse(@expected)
   end
   
   it "should return a copy of time" do
-    time = datetime.to_time
+    time = @datetime.to_time
     time.class.should == Time
     time.to_s.should == @expected
   end
@@ -16,6 +16,7 @@ end
 
 describe Time, "#to_datetime" do
   it "should return a copy of its self" do
+    datetime = DateTime.now
     datetime.to_datetime.should == datetime
   end
 end
