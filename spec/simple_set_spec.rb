ce17ce7b7735e7972a34ccb98411ce4d3a81cf49
Michael S. Klishin <michael@novemberain.com>
2008-11-06 01:03:42 +0200

    Foos and bars suck, never use them

--- spec/simple_set_spec.rb
+++ spec/simple_set_spec.rb
@@ -1,26 +1,25 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Extlib::SimpleSet do
-  
+
   before do
     @s = Extlib::SimpleSet.new("Foo")
   end
-  
+
   it "should support <<" do
     @s << "Hello"
-    @s.should be_include("Hello")
+    @s.to_a.should include("Hello")
   end
-  
+
   it "should support merge" do
-    @s.should have_key("Foo")
-    @t = @s.merge(["Bar"])
+    @s.should have_key("Before merge")
+    @t = @s.merge(["Merged value"])
     @t.should be_kind_of(Extlib::SimpleSet)
-    @t.should have_key("Foo")
-    @t.should have_key("Bar")    
+    @t.should have_key("Before merge")
+    @t.should have_key("Merged value")
   end
-  
+
   it "should support inspect" do
     @s.inspect.should == "#<SimpleSet: {\"Foo\"}>"
   end
-  
 end
