2bc2e8a42c7a49e2e5daf530c29fb2840d0e299d
Dan Kubb <dan.kubb@autopilotmarketing.com>
2009-01-20 11:18:29 -0800

    Updated Module.find_const to use const_missing if constant not found
    * Delegate to Object.full_const_get when constant not found ensures
      that const_missing will execute.  If a const_missing handler is
      present (like with autoload), this may allow the constant to be
      defined by other means.

--- lib/extlib/module.rb
+++ lib/extlib/module.rb
@@ -1,7 +1,7 @@
 class Module
   def find_const(const_name)
     if const_name[0..1] == '::'
-      Object.find_const(const_name[2..-1])
+      Object.full_const_get(const_name[2..-1])
     else
       nested_const_lookup(const_name)
     end
@@ -15,31 +15,33 @@ class Module
 
   # Doesn't do any caching since constants can change with remove_const
   def nested_const_lookup(const_name)
-    constants = [ Object ]
+    unless equal?(Object)
+      constants = []
 
-    unless self == Object
-      self.name.split('::').each do |part|
-        constants.unshift(constants.first.const_get(part))
+      name.split('::').each do |part|
+        const = constants.first || Object
+        constants << const.const_get(part)
       end
-    end
 
-    parts = const_name.split('::')
+      parts = const_name.split('::')
 
-    # from most to least specific constant, use each as a base and try
-    # to find a constant with the name const_name within them
-    constants.each do |const|
-      # return the nested constant if available
-      return const if parts.all? do |part|
-        const = if RUBY_VERSION >= '1.9.0'
-          const.const_defined?(part, false) ? const.const_get(part, false) : nil
-        else
-          const.const_defined?(part) ? const.const_get(part) : nil
+      # from most to least specific constant, use each as a base and try
+      # to find a constant with the name const_name within them
+      constants.reverse_each do |const|
+        # return the nested constant if available
+        return const if parts.all? do |part|
+          const = if RUBY_VERSION >= '1.9.0'
+            const.const_defined?(part, false) ? const.const_get(part, false) : nil
+          else
+            const.const_defined?(part) ? const.const_get(part) : nil
+          end
         end
       end
     end
 
-    # if we get this far then the nested constant was not found
-    raise NameError, "uninitialized constant #{const_name}"
+    # no relative constant found, fallback to an absolute lookup and
+    # use const_missing if not found
+    Object.full_const_get(const_name)
   end
 
 end # class Module
