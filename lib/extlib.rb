04e21971cc4403e720c88b27d26d36c38a883c75
Dan Kubb <dan.kubb@autopilotmarketing.com>
2009-02-24 10:49:43 -0800

    Added Array#to_hash and Array#to_mash
    * Moved from dm-core/next

--- lib/extlib.rb
+++ lib/extlib.rb
@@ -9,18 +9,19 @@ require File.expand_path(File.join(__DIR__, 'extlib', 'pathname'))
 
 dir = Pathname(__FILE__).dirname.expand_path / 'extlib'
 
-require dir / "class.rb"
-require dir / "object"
-require dir / "object_space"
+require dir / 'class.rb'
+require dir / 'object'
+require dir / 'object_space'
 
-require dir / "string"
-require dir / "symbol"
-require dir / "hash"
-require dir / "mash"
-require dir / "virtual_file"
-require dir / "logger"
-require dir / "time"
-require dir / "datetime"
+require dir / 'array'
+require dir / 'string'
+require dir / 'symbol'
+require dir / 'hash'
+require dir / 'mash'
+require dir / 'virtual_file'
+require dir / 'logger'
+require dir / 'time'
+require dir / 'datetime'
 
 require dir / 'assertions'
 require dir / 'blank'
@@ -35,13 +36,13 @@ require dir / 'simple_set'
 require dir / 'struct'
 require dir / 'symbol'
 
-Extlib.autoload("Hook", (dir / 'hook').to_s)
-Extlib.autoload("Pooling", (dir / 'pooling').to_s)
+Extlib.autoload('Hook', (dir / 'hook').to_s)
+Extlib.autoload('Pooling', (dir / 'pooling').to_s)
 
 module Extlib
 
   def self.exiting= bool
-    if bool && Extlib.const_defined?("Pooling")
+    if bool && Extlib.const_defined?('Pooling')
       if Extlib::Pooling.scavenger?
         Extlib::Pooling.scavenger.wakeup
       end
