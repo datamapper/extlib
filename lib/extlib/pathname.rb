bde21c42f24341276fcabbcb30ca3c1f2ab92753
Dan Kubb <dan.kubb@autopilotmarketing.com>
2009-03-23 15:00:34 -0700

    Alias Pathname#to_s to Pathname#to_str when not defined
    * Allows Pathname objects to be used with require statements

--- lib/extlib/pathname.rb
+++ lib/extlib/pathname.rb
@@ -12,4 +12,9 @@ class Pathname
   def /(path)
     (self + path).expand_path
   end
+
+  # alias to_s to to_str when to_str not defined
+  unless public_instance_methods(false).any? { |m| m.to_s == 'to_str' }
+    alias to_str to_s
+  end
 end # class Pathname
