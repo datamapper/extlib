c08caaff8a33b046bdb6a5d7305e30706dc3a4dc
Dirkjan Bussink <d.bussink@gmail.com>
2009-03-30 19:24:42 +0200

    Move ByteArray to Extlib module

--- lib/extlib/byte_array.rb
+++ lib/extlib/byte_array.rb
@@ -1,4 +1,6 @@
 # This class has exists to represent binary data. This is mainly
 # used by DataObjects. Binary data sometimes needs to be quoted differently
 # than regular string data (even if the string is just plain ASCII).
-class ByteArray < String; end
+module Extlib
+  class ByteArray < ::String; end
+end
